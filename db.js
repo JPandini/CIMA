85129d6bf75378d312838d68ac75a1cbf4c28ffa
João Vitor Pandini <99928050+JPandini@users.noreply.github.com>
2023-08-15 22:51:39 -0300

    banco

--- db.js
+++ db.js
@@ -1,3 +1,21 @@
 const mysql = require("mysql2/promise");
 
 const client = mysql.createPool(process.env.CONNECTION_STRING);
+
+async function selectCustomers() {
+    const results = await client.query("SELECT * FROM clientes;"); //await só vai pra linha de baixo quando é executado
+    return results[0];
+}
+
+/* async function selectCustomers(id) {
+    const results = await client.query("SELECT * FROM clientes WHERE id=?;", [id]);
+    return results[0];
+} */
+
+
+
+
+
+module.exports = {selectCustomers,
+
+}
--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 require('dotenv').config();
+const db = require('./db');
 
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
+app.delete("/clientes/:id", (req, res)=>{
+    const id = parseInt(res.params.id);
+    db.deleteCostumer(id);
+    response.sendStatus(204);
+})
+
+app.patch("/clientes/:id", (req, res)=>{
+    const id = parseInt(res.params.id);
+    const costumers = request.body;
+    db.updateCostumer(id, costumers);
+    res.sendStatus(200);
+})
+
+app.post("/clientes", (req, res)=>{
+    const costumers = request.body;
+    db.insertCostumer(costumers);
+    res.sendStatus(201);
+})
+
+app.get("/clientes/:id", async (req, res)=>{
+    const id = parseInt(res.params.id);
+    const results = await db.selectCustomers(id);
+    res.json(results);
+}) 
+
+app.get("/clientes", async (req, res)=>{
+    const results = await db.selectCustomers();
+    res.json(results);
+})
+
 
 app.get("/", (req, res)=>{
     res.json({message:"hello"})
