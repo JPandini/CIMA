5e34bb619ebae599d7ee00fca00765706e33558a
João Vitor Pandini <99928050+JPandini@users.noreply.github.com>
2023-09-11 17:15:15 -0300

    testeBancoPhp

--- db.js
+++ db.js
@@ -1,9 +1,15 @@
 const mysql = require("mysql2/promise");
-const client = mysql.createPool(process.env.CONNECTION_STRING);
+/* const client = mysql.createPool(process.env.CONNECTION_STRING); */
+const client = mysql.createPool({
+    host     : '%10.107.0.32',
+    user     : 'root',
+    password : 'cima',
+    database : 'cima'
+  });
 
 async function selectCustomers() {
     const results = await client.query("SELECT * FROM clientes;");
-    return results[0];
+    return results[0]; 
 }
 
 async function selectCustomer(id) { 
