9ea63a596dc3ccd269d909634b06e19f8009c370
João Vitor Pandini <99928050+JPandini@users.noreply.github.com>
2023-10-07 13:52:29 -0300

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const client = mysql.createPool({
     host     : 'localhost',
     user     : 'root',
-    password : '',
+    password : 'admin1234',
     database : 'cima'
 }); 
 
